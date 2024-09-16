7bbf3d085510ebe6fa8b9d1b7629bc437af545f1
Vamsi Ravula <codingsaduvu@gmail.com>
2024-09-16 14:54:22 -0700

    arm64 changes and push payment processor code

---
+++ payment-processor/src/main/java/net/example/PaymentProcessor.java
@@ -0,0 +1,17 @@
+package net.example;
+
+import org.apache.camel.builder.RouteBuilder;
+
+public class PaymentProcessor extends RouteBuilder {
+    @Override
+    public void configure() throws Exception {
+        rest("/api/pay")
+            .get()
+            .produces("text/plain")
+            .route()
+            .to("braintree://clientToken/generate")
+            .setBody(simple("${env.HOSTNAME}"))
+            .log("${body}")
+            .endRest();
+    }
+}
